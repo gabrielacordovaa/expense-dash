12e7953f3330f84432c17b98458823c695b80136
F0129322 <Gab.cor9>
2021-11-20 17:02:33 -0300

    expense item and date components initialized

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
+import ExpenseItem from "./components/expense-item/ExpenseItem.js";
 function App() {
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
+
   return (
-    <div>
-      <h1>Hi</h1>
+    <div>      
+      <ExpenseItem 
+        title={expenses[0].title}
+        date={expenses[0].date}
+        amount={expenses[0].amount}
+      />
     </div>
   );
 }
